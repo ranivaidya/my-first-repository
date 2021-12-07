a65851ec969ec866ff83410661e231bc6b61826d
ranivaidya <vaidyarani8@gmail.com>
2021-12-07 23:21:44 +0530

    first commit

---
+++ DuplicateElement.java
@@ -0,0 +1,11 @@
+public class DuplicateElement{
+public static void main(String []args){
+int[] arr ={3,4,5,6,7,6,54,45,4,3,7};
+for (int i=0; i<arr.length; i++){
+for(int j=i+1; j<arr.length; j++){
+if(arr[i]==arr[j])
+System.out.print(" "+arr[j]);
+}
+}
+}
+}
