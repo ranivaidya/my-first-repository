4241d2565ce3b75d3fc2edd47f3f88192e5d9bc1
ranivaidya <95687035+ranivaidya@users.noreply.github.com>
2021-12-08 11:17:41 +0530

    Update DuplicateElement.java
    change the element

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -1,6 +1,6 @@
 public class DuplicateElement{
 public static void main(String []args){
-int[] arr ={3,4,5,6,8,6,54,45,5,3,7};
+int[] arr ={8,4,5,6,8,6,60,45,5,3,7};
 for (int i=0; i<arr.length; i++){
 for(int j=i+1; j<arr.length; j++){
 if(arr[i]==arr[j])
